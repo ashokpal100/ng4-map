ad3b22bea934cc4162a30517c7c46ae5bf896322
EC2 Default User <ec2-user@ip-172-31-2-176.ap-southeast-1.compute.internal>
2018-02-07 19:37:22 +0000

    imp google map by Angular Google Maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+import { AgmCoreModule } from '@agm/core';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
